aaaf03e5c70a897c6fd0b144b245b8f63e8d3be0
Pierre_Junior <sibafwafpj@gmail.com>
2023-11-30 22:06:59 -0500

    :hammer: Page d'inscription en cours

--- app/src/main/java/com/example/tp4/ConnexionInscriptionActivity.kt
+++ app/src/main/java/com/example/tp4/ConnexionInscriptionActivity.kt
@@ -1,33 +1,41 @@
 package com.example.tp4
 
+import android.R
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.setPadding
-import com.example.tp4.databinding.ConnexionBinding
-import com.example.tp4.ui.home.HomeFragment
-import com.example.tp4.ui.inscription.InscriptionFragment
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import com.example.tp4.databinding.ConnexionInscriptionBinding
+import com.example.tp4.R.id.nav_host_fragment_content
+import com.example.tp4.R.id.nav_connexion
+import com.example.tp4.R.id.nav_details
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
-import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import com.google.firebase.auth.FirebaseUser
 
-class ConnexionInscriptionActivity: AppCompatActivity(), View.OnClickListener {
-    private lateinit var binding: ConnexionBinding
-    private lateinit var textViewEmail:TextView
-    private lateinit var textViewPassword:TextView
-    private lateinit var textError:TextView
+
+class ConnexionInscriptionActivity: AppCompatActivity() {
+    private lateinit var binding: ConnexionInscriptionBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private var mAuth: FirebaseAuth? = null
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ConnexionBinding.inflate(layoutInflater)
+        binding = ConnexionInscriptionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // Instance du service d'authentification
@@ -42,64 +50,26 @@ class ConnexionInscriptionActivity: AppCompatActivity(), View.OnClickListener {
             updateUi(mAuth!!.currentUser)
         }
 
-        textViewEmail=binding.editTextTextEmail
-        textViewPassword=binding.editTextTextPassword
-        textError=binding.textError
-        binding.textView2.setOnClickListener(this)
+        setSupportActionBar(binding.appBarMain.toolbar)
 
-        // Connexion
-        binding.buttonConnexion.setOnClickListener(this)
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView2
 
-    }
-    override fun onClick(v: View) {
-        when (v.id) {
-            R.id.textView2 -> {
-                startActivity(Intent(this@ConnexionInscriptionActivity, InscriptionFragment::class.java))
-            }
-            R.id.button_connexion -> {
-                signIn(textViewEmail.getText().toString().trim(),
-                    textViewPassword.getText().toString().trim())
-            }
-        }
+        val navController = findNavController(nav_host_fragment_content)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                nav_connexion
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 
-    private fun signIn(courriel: String, mdp: String) {
-
-        mAuth!!.signInWithEmailAndPassword(courriel, mdp)
-            .addOnCompleteListener(
-                this
-            ) { task ->
-                Toast.makeText(
-                    this, "Connexion en cours ...",
-                    Toast.LENGTH_SHORT,
-                ).show()
-                if (task.isSuccessful) {
-                    // Firebase User est un user connecté
-                    val user = mAuth!!.currentUser
-                    Toast.makeText(
-                        this, "Connexion réussie",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                    updateUi(user)
-
-                }/* else {
-                    Log.w(
-                        SignInActivity.Companion.TAG,
-                        "signInWithEmail:failure",
-                        task.exception
-                    )
-                }*/
-            } // Ce callback nous permet d'avoir accès aux exceptions
-            // Important, car permet l'identification exacte de l'erreur à la connexion
-            .addOnFailureListener { e ->
-                textError.setPadding(20)
-                textError.backgroundTintList= ContextCompat.getColorStateList(this, R.color.error)
-                if (e is FirebaseAuthInvalidCredentialsException || e is FirebaseAuthInvalidUserException) {
-                    textError.setText("Email ou mot de passe invalide")
-                }else {
-                    textError.setText("Échec de connexion")
-                }
-            }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(nav_host_fragment_content)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     private fun updateUi(user: FirebaseUser?) {
@@ -110,11 +80,8 @@ class ConnexionInscriptionActivity: AppCompatActivity(), View.OnClickListener {
         /*if (user.isEmailVerified == true) binding.textView6.setText("Courriel vérifié") else binding.textView6.setText(
             "Courriel non vérifié"
         )*/
-        startActivity(Intent(this@ConnexionInscriptionActivity, MainActivity::class.java))
-    }
-
-    override fun onStop() {
-        super.onStop()
-        FirebaseAuth.getInstance().signOut()
+        startActivity(Intent(this, MainActivity::class.java))
     }
 }
+
+
--- app/src/main/java/com/example/tp4/ui/inscription/InscriptionFragment.kt
+++ app/src/main/java/com/example/tp4/ui/inscription/InscriptionFragment.kt
@@ -1,6 +1,149 @@
 package com.example.tp4.ui.inscription
 
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.tp4.MainActivity
+import com.example.tp4.R
+import com.example.tp4.databinding.ConnexionBinding
+import com.example.tp4.databinding.InscriptionBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
 
 class InscriptionFragment:Fragment() {
+    private var _binding: InscriptionBinding? = null
+
+    private val binding get() = _binding!!
+    private var mAuth: FirebaseAuth? = null
+
+    private lateinit var txtViewName:EditText
+    private lateinit var txtViewEmail:EditText
+    private lateinit var txtViewMdp:EditText
+    private lateinit var textError:TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = InscriptionBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        // Instance du service d'authentification
+        mAuth = FirebaseAuth.getInstance()
+
+        // Si token user déjà valide
+        if (mAuth!!.currentUser != null) {
+            Log.d("***TAG", "onCreate: " + mAuth!!.currentUser)
+            //Rediriger vers l'accueil
+
+            Toast.makeText(requireContext(), "Vous êtes déjà connecté.", Toast.LENGTH_SHORT).show()
+            updateUi(mAuth!!.currentUser)
+        }
+
+        txtViewEmail=binding.editTextTextEmail
+        txtViewMdp=binding.editTextTextPassword
+        txtViewName=binding.editTextTextPersonName
+        textError=binding.textErrorInscription
+
+        binding.buttonSignup.setOnClickListener{
+            if (txtViewEmail.text.toString().trim().isNullOrEmpty()||txtViewName.text.toString().trim().isNullOrEmpty()||txtViewMdp.text.toString().trim().isNullOrEmpty())
+                errorWithMessage("Veuillez bien remplir tous les champs")
+            else
+                createAccount(txtViewName.text.toString().trim(),txtViewEmail.text.toString().trim(),txtViewMdp.text.toString().trim())
+        }
+        return root
+
+    }
+
+
+    // Inscription
+    private fun createAccount(nom: String, courriel: String, mdp: String) {
+        // mode courriel + mdp : noter qu'on ne peut pas passer un username...
+        mAuth!!.createUserWithEmailAndPassword(courriel, mdp)
+            .addOnCompleteListener(
+                requireActivity()
+            ) { task ->
+                if (task.isSuccessful) {
+                    // Succès, on a désormais accès a l'objet utilisateur
+                    Log.d("***TAG", "createUserWithEmail:success")
+                    val user = mAuth!!.currentUser
+
+                    // ...Et on peut construire un profil associé à un utilisateur connecté
+                    val profileUpdates = UserProfileChangeRequest.Builder()
+                        .setDisplayName(nom) // on peut également ajouter une photo avec .setPhotoUri(Uri uri)
+                        .build()
+                    user!!.updateProfile(profileUpdates)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Log.d("TAG", "Profil mis à jour.")
+                                Toast.makeText(
+                                    requireContext(), "Bienvenue ${user.displayName}",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                updateUi(user)
+                            }
+                        }
+                } /*else {
+                    // Échec : en cas d'échec, vérifier dans le logcat l'instance d'exception lancée
+                    Log.w(
+                        TAG,
+                        "createUserWithEmail:failure",
+                        task.exception
+                    )
+                    Toast.makeText(
+                        this@SignUpActivity, "Échec de l'Authentication.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }*/
+            }
+            .addOnFailureListener { e ->
+                if(e is FirebaseAuthWeakPasswordException)
+                    errorWithMessage("Le mot de passe est trop faible. Il doit contenir au moins 6 caractères.")
+                else if(e is FirebaseAuthInvalidCredentialsException)
+                    errorWithMessage("Adresse e-mail invalide.")
+                else if(e is FirebaseAuthUserCollisionException)
+                    errorWithMessage("Un compte avec cette adresse e-mail existe déjà.")
+                else
+                    errorWithMessage("Échec de l'inscription. Veuillez réessayer plus tard.")
+            }
+        // Ici, on pourrait ajouter un callback .addOnFailureListener
+    }
+
+
+    private fun errorWithMessage(msgError:String){
+        textError.setPadding(20,20,20,20)
+        textError.backgroundTintList= ContextCompat.getColorStateList(requireContext(), R.color.error)
+        textError.setText(msgError)
+    }
+
+    private fun updateUi(user: FirebaseUser?) {
+        // token retourné par Firebase : utile si on doit par la suite communiquer avec un backend tierce
+        //binding.textView5.setText("Bienvenue "+user.getIdToken(true));
+        //binding.textView5.setText("Bienvenue " + user!!.displayName)
+
+        /*if (user.isEmailVerified == true) binding.textView6.setText("Courriel vérifié") else binding.textView6.setText(
+            "Courriel non vérifié"
+        )*/
+        startActivity(Intent(requireContext(), MainActivity::class.java))
+    }
+
+    override fun onStop() {
+        super.onStop()
+        FirebaseAuth.getInstance().signOut()
+    }
+
 }
